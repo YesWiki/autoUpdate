85641d9bfe9d8f34994c4991bdb9188fec086f83
Florestan Bredow <daiko@daiko.fr>
2016-03-11 18:44:01 +0100

    Externalise la gestion du dépot par defaut

--- app/Repository.php
+++ app/Repository.php
@@ -3,79 +3,13 @@ namespace AutoUpdate;
 
 class Repository
 {
-    const DEFAULT_REPO = 'http://yeswiki.net/repository/stable/';
 
     private $address;
     private $data = null;
 
-    public function __construct($address = null)
+    public function __construct($address)
     {
-        if (is_null($address)) {
-            $address = $this::DEFAULT_REPO;
-        }
         $this->address = $address;
-    }
-
-    /**
-     * [compareVersion description]
-     * @param  [type] $local_version [description]
-     * @param  string $repo_version  [description]
-     * @return [type]                [description]
-     */
-    public function compareVersion($localVersion, $repoVersion = 'stable')
-    {
-        $repoVersion = $this->getVersion($repoVersion);
-
-        if ($localVersion === $repoVersion) {
-            return 0;
-        }
-
-        $repoVersion = $this->evalVersion($repoVersion);
-
-        $localVersion = $this->evalVersion(
-            $localVersion
-        );
-
-        for ($i = 0; $i < 3; $i++) {
-            if ($repoVersion[$i] > $localVersion[$i]) {
-                return $i + 1;
-            }
-        }
-        return -1;
-    }
-
-    public function getVersion($repoVersion = 'stable')
-    {
-        return $this->data[$repoVersion]['version'];
-    }
-
-    public function getMD5($version = 'stable')
-    {
-        $disMd5File = file_get_contents(
-            $this->address . $this->data[$version]['file'] . '.md5'
-        );
-        return explode('  ', $disMd5File)[0];
-    }
-
-    public function getFile($version = 'stable')
-    {
-        if ('unstable' !== $version) {
-            $version = 'stable';
-        }
-
-        $destinationFile = tempnam(sys_get_temp_dir(), 'yeswiki_');
-        $sourceUrl = $this->address . $this->data[$version]['file'];
-
-        $this->downloadFile(
-            $sourceUrl,
-            $destinationFile
-        );
-
-        if (is_file($destinationFile)) {
-            return $destinationFile;
-        }
-
-        return false;
     }
 
     public function load()
@@ -98,6 +32,62 @@ class Repository
         }
 
         return true;
+    }
+
+    /**
+     * [compareVersion description]
+     * @param  [type] $local_version [description]
+     * @param  string $repo_version  [description]
+     * @return [type]                [description]
+     */
+    public function compareVersion($localVersion)
+    {
+        $repoVersion = $this->getVersion();
+
+        if ($localVersion === $repoVersion) {
+            return 0;
+        }
+
+        $repoVersion = $this->evalVersion($repoVersion);
+        $localVersion = $this->evalVersion($localVersion);
+
+        for ($i = 0; $i < 3; $i++) {
+            if ($repoVersion[$i] > $localVersion[$i]) {
+                return $i + 1;
+            }
+        }
+        return -1;
+    }
+
+    public function getVersion()
+    {
+        return $this->data['version'];
+    }
+
+    public function getMD5()
+    {
+        $disMd5File = file_get_contents(
+            $this->address . $this->data['file'] . '.md5'
+        );
+        return explode('  ', $disMd5File)[0];
+    }
+
+    public function getFile()
+    {
+
+        $destinationFile = tempnam(sys_get_temp_dir(), 'yeswiki_');
+        $sourceUrl = $this->address . $this->data['file'];
+
+        $this->downloadFile(
+            $sourceUrl,
+            $destinationFile
+        );
+
+        if (is_file($destinationFile)) {
+            return $destinationFile;
+        }
+
+        return false;
     }
 
     private function evalVersion($version)
